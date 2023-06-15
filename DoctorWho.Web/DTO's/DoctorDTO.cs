db52b877650505cfa27db4387b5529ab595d4458
Mhijazi16 <gultimate0@gmail.com>
2023-06-15 12:57:28 +0300

    Controllers : create DoctorDTO to handle object transfer.

---
+++ DoctorWho.Web/DTO's/DoctorDTO.cs
@@ -0,0 +1,10 @@
+namespace DoctorWho.Web.DTO_s;
+
+public class DoctorDTO
+{
+    public String? Name { get; set; }
+    public int? Number { get; set; }
+    public DateTime? LastEpisodeDate { get; set; }
+    public DateTime? FirstEpisodeDate { get; set; } 
+    public DateTime? BirthDay { get; set; }
+}
