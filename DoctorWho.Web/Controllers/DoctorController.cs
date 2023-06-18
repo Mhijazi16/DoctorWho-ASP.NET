33aa3c8a6f77e077ae11ffb223808b99c12626d8
Mhijazi16 <gultimate0@gmail.com>
2023-06-18 12:39:18 +0300

    Controllers : Create GetDoctor(); to handle GET requests from /api/doctors/{id}

--- DoctorWho.Web/Controllers/DoctorController.cs
+++ DoctorWho.Web/Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using DoctorWho.Db;
 using DoctorWho.Db.Migrations;
 using DoctorWho.Web.DTO_s;
 using DoctorWhoRepository;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoctorWho.Web.Controllers;
@@ -26,9 +27,21 @@ public class DoctorController : Controller
         var doctors = await _repository.GetAllDoctorsAsync();
         if (doctors == null)
         {
-            return NotFound();
+            return NotFound("There are no Doctors");
         }
 
         return Ok(_mapper.Map<IEnumerable<DoctorDTO>>(doctors));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<DoctorDTO>> GetDoctor(int id)
+    {
+       var doctor = await _repository.GetDoctorAsync(id);
+       if (doctor == null)
+       {
+           return NotFound("Docotr Does not Exist.");
+       }
+
+       return Ok(_mapper.Map<DoctorDTO>(doctor));
     }
 }
